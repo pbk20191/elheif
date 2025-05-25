f491fb1c9173b65a58fbfdf39b8eed9e1e9fdd46
park-byeong-gwan <impbk2002@gmail.com>
2025-05-25 20:48:15 +0900

    add moduleProp

--- pkg/index.d.ts
+++ pkg/index.d.ts
@@ -1,8 +1,20 @@
-import MainModuleFactory from './elheif-wasm';
+import Factory from './elheif-wasm';
 
 // TypeScript bindings for emscripten-generated code.  Automatically generated at compile time.
 export * from './elheif-wasm';
 
+export interface WasmModuleProp {
 
-// export { default as MainModuleFactory } from './elheif-wasm';
+    onRuntimeInitialized?: () => void
+    onAbort?: (tag:string|number|undefined) => void;
+    locateFile?:(file:string) => string;
+    monitorRunDependencies?: (left:number) => void;
+    onExit?: (code: number) => void;
+    preInit?: (() => void);
+    preRun?: (() => void);
+    postRun?: (() => void);
+}
+
+export function MainModuleFactory(options?: WasmModuleProp): ReturnType<typeof Factory>
+
 export default MainModuleFactory;
